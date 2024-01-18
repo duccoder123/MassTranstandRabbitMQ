b39b24c8ca65fb905a7ceacea3f476004608a58b
dang2303 <8duchuynh@gmail.com>
2024-01-18 22:36:23 +0700

    addAuctionServiceandMore

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,92 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    [ApiController]
+    [Route("api/auctions")]
+    public class AuctionsController : ControllerBase
+    {
+private readonly AuctionDbContext _context;
+private readonly IMapper _mapper;
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+       [HttpGet]
+       public async Task<ActionResult<List<AuctionDTO>>> GetAllAuctions (string date){
+        var auction = await _context.Auctions
+        .Include(x => x.Item)
+        .OrderBy(x => x.Item.Make)
+        .ToListAsync();
+        return _mapper.Map<List<AuctionDTO>>(auction);
+        // var query = _context.Auctions.OrderBy(  x => x.Item.Make).AsQueryable();
+        // if(!string.IsNullOrEmpty(date)){
+        //     query = query.Where( x=> x.UpdateAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0); 
+        // }
+        //         return await query.ProjectTo<AuctionDTO>(_mapper.ConfigurationProvider).ToListAsync();
+       }
+
+       [HttpGet("{id}")]
+       public async Task<ActionResult<AuctionDTO>> GetAuctionById(Guid id)
+       {
+                var auction = await _context.Auctions
+                                    .Include( x=> x.Item)
+                                    .FirstOrDefaultAsync( x => x.Id == id);
+                
+                if(auction == null) return NotFound();
+                return _mapper.Map<AuctionDTO>(auction);
+       }
+       [HttpPost]
+       public async Task<ActionResult<AuctionDTO>> CreateAuction (CreateAuctionDTO auctionDto)
+       {
+            var auction = _mapper.Map<Auction>(auctionDto);
+            auction.Seller= "test";
+            // User.Identity.Name;
+            _context.Auctions.Add(auction);
+                       var newAuction = _mapper.Map<AuctionDTO>(auction);
+            var result = await _context.SaveChangesAsync() > 0;
+ 
+            if(!result) return BadRequest("Could not save changes to the DB");
+            return CreatedAtAction(nameof(GetAuctionById), 
+                        new {auction.Id}, _mapper.Map<AuctionDTO>(auction));
+       }
+        [HttpPut("{id}")]
+       public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDTO updateAuctionDto)
+       {
+            var auction = await _context.Auctions.Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            
+            // if(auction == null) return NotFound();
+            // if(auction.Seller != User.Identity.Name) return Forbid();
+
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+          var result = await _context.SaveChangesAsync() > 0;
+            if(result) return Ok();
+            return BadRequest("Problem saving changes");
+       }
+       [HttpDelete("{id}")]
+       public async Task<ActionResult> DeleteAuction(Guid id)
+       {
+            var auction = await _context.Auctions.FindAsync(id);
+            if(auction == null) return NotFound();
+            // if(auction.Seller != User.Identity.Name) return Forbid();
+            _context.Auctions.Remove(auction);
+            var result = await _context.SaveChangesAsync() > 0;
+            if(!result) return BadRequest("Could not update DB");
+            return Ok();
+        }
+    }
+}
